77dcefe5aa697949d73dcec23620d7ae1bb63e24
ingebrigtfb <ingboe00998@stud.noroff.no>
2024-10-04 14:07:29 +0200

    Add GitHub Actions

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_URL = 'https://api.nasa.gov/planetary/apod';
-const API_KEY = process.env.NASA_API_KEY; // Access the API Key from environment variables
+const API_KEY = process.env.VITE_NASA_API_KEY; // Access the API Key from environment variables
 export async function getData() {
   try {
     const response = await axios.get(API_URL, {
